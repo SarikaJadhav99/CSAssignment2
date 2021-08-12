19b2b2e9cbfeaf3eb321dfeb13c96c7a28044911
SarikaJadhav99 <88771229+SarikaJadhav99@users.noreply.github.com>
2021-08-12 21:19:04 +0530

    Add project files.

---
+++ CSAssignment2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Pattern
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // define string
+            String str = "PRORIGO";
+
+            Console.WriteLine("String : " + str);
+
+            // To print odd index characters
+            for (int i = 1; i <= str.Length; i += 2)
+            {
+                Console.WriteLine(str.Substring(0, i));
+               
+            }
+
+         Console.ReadLine();
+        }
+    }
+}
