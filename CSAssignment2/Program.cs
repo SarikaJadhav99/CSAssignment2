35b378243036f079e2e352a13edbd13d62190e25
SarikaJadhav99 <88771229+SarikaJadhav99@users.noreply.github.com>
2021-08-13 20:17:27 +0530

    please check code

--- CSAssignment2/Program.cs
+++ CSAssignment2/Program.cs
@@ -1,22 +1,30 @@
 ﻿using System;
 
 namespace Pattern
-
 {
-    class Program
+    public class printPattern
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            // define string
-            String str = "PRORIGO";
+         // define string
+         string str = "PRORIGO";
 
-            Console.WriteLine("String : " + str);
+         Console.WriteLine("String : " + str);
 
-            // To print odd index characters
-            for (int i = 1; i <= str.Length; i += 2)
+         // To print odd index characters
+         for (int i = 1; i <= str.Length; i += 2)
             {
                 Console.WriteLine(str.Substring(0, i));
-               
+            }
+
+           //To print pyramid pattern
+            for (int j = 1; j <= str.Length; j+=2)
+            {
+                for(int k= str.Length-j; k>0;k-- )
+                {
+                    Console.Write(" ");
+                }
+                Console.WriteLine(" "+ str.Substring(0,j)+" ");  // HERE IS THE PROBLEM 
             }
 
          Console.ReadLine();
